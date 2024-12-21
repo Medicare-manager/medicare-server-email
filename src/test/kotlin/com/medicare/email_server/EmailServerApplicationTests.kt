e1cba91fcd1451e817cb98217d491b74fec4cc27
silvia <silviafernandes974@gmail.com>
2024-12-21 17:16:25 -0300

    Mensagem do commit

---
+++ src/test/kotlin/com/medicare/email_server/EmailServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.medicare.email_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmailServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
