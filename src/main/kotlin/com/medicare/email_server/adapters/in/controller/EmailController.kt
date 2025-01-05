cf743a1f39fb2303b4145d1db5006f06b37e3e8d
silvia <silviafernandes974@gmail.com>
2025-01-05 20:02:46 -0300

    feat: adição de documentação

--- src/main/kotlin/com/medicare/email_server/adapters/in/controller/EmailController.kt
+++ src/main/kotlin/com/medicare/email_server/adapters/in/controller/EmailController.kt
@@ -8,10 +8,37 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * REST controller for managing email sending.
+ *
+ * This class provides an endpoint for sending emails using the `EmailService`.
+ * Clients can send requests to send an email by specifying the recipient, subject, and body of the message.
+ *
+ * Available endpoints:
+ * - POST /api/emails/send: Sends an email based on the provided request data.
+ *
+ * @param emailService The service responsible for handling email sending operations.
+ */
 @RestController
 @RequestMapping("/api/emails")
 class EmailController(private val emailService: EmailService) {
 
+    /**
+     * Endpoint to send emails.
+     *
+     * This method handles POST requests with the details of the email provided in the request body.
+     * If the email is sent successfully, it returns a success message.
+     * If an error occurs during the sending process, it returns an error message with the reason.
+     *
+     * @param request An object containing the email details to be sent, including:
+     * - `to`: The recipient's email address.
+     * - `subject`: The subject of the email.
+     * - `body`: The body of the email.
+     *
+     * @return ResponseEntity containing:
+     * - Status 200 (OK) and a success message if the email is sent successfully.
+     * - Status 400 (Bad Request) and an error message if there is a failure during the sending process.
+     */
     @PostMapping("/send")
     fun sendEmail(@RequestBody request: EmailMessage): ResponseEntity<String> {
         return try {
